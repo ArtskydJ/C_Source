04a20d1d0d9c1faf372ba95dd8d652d4a3fec158
Joseph Dykstra <josephdykstra@gmail.com>
2013-09-19 10:23:08 -0500

    How To update

--- HowTo/HowTo.c
+++ HowTo/HowTo.c
@@ -5,7 +5,7 @@
 
 #include "SDL/SDL.h"
 #include "SDL/SDL_ttf.h"
-#include "EasySDL.h"
+#include "including/EasySDL.h"
 #define SCR_WIDTH	640
 #define SCR_HEIGHT	480
 #define SCR_BPP		32
@@ -35,18 +35,18 @@ int main( int argc, char* args[] )
 	EZ_set_rect(&drawrect,100,100,20,20);
 	SDL_FillRect(screen, &drawrect, 0x0000FF);
 	SDL_Flip(screen);
-	SDL_Delay(100);
+	//SDL_Delay(100);
 	
 	EZ_apply_rect(screen, EZ_new_rect(100,200,20,20), 0x0000FF);
 	SDL_Flip(screen);
-	SDL_Delay(100);
+	//SDL_Delay(100);
 	
 	EZ_apply_rect(screen, EZ_new_rect(200,200,20,20), EZ_new_Uint32(000,000,255));
 	SDL_Flip(screen);
-	SDL_Delay(100);
+	//SDL_Delay(100);
 	
 	EZ_apply_text(screen,"hello",    font,EZ_new_rect(10,0,0,0), EZ_new_color(128,128,128));
-	EZ_apply_text(screen,"what's up",font,EZ_new_rect(10,50,0,0),EZ_Uint32_to_SDL(0x00FF00));
+	EZ_apply_text(screen,"what's up?",font,EZ_new_rect(10,50,0,0),EZ_Uint32_to_SDL(0x00FF00));
 	SDL_Flip(screen);
 	
 	EZ_waitForExit();
